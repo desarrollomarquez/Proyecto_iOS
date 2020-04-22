55a29991b7d48f00c4165c3f912687b41fc259b6
Diego Marquez <diego.marquezbetancur@tcs.com>
2020-04-22 14:53:36 -0500

    Adicion tipos variables

--- Varios/tutoSwift.playground/Contents.swift
+++ Varios/tutoSwift.playground/Contents.swift
@@ -1,4 +1,50 @@
 import UIKit
 
+/*
 var str = "Hello, playground"
-print(str)
+print(str) */
+
+
+/* Alias de tipo */
+typealias Feet = Int
+var distance: Feet = 100
+print("Resultado: ", distance)
+
+// varA is inferred to be of type Int
+var varA = 42
+print(varA)
+
+// varB is inferred to be of type Double
+var varB = 3.14159
+print(varB)
+
+// varC is also inferred to be of type Double
+var varC = 3 + 0.14159
+//var varD = varA + varB
+print(varC)
+
+var varA1 = "Godzilla"
+var varB2 = 1000.00
+
+print("Value of \(varA1) is more than \(varB2) millions")
+
+//opcionales
+
+var myString:String? = "Holli"//"Holli"
+
+if myString != nil {
+    print(myString)
+} else {
+    print("myString has nil value","\n")
+}
+
+
+var myString2:String? = "Hello, Swift 4!"
+//myString2 = "Hello, Swift 4!"
+
+if let yourString = myString2 {
+    print("Your string has - \(yourString)")
+} else {
+    print("Your string does not have a value")
+}
+
